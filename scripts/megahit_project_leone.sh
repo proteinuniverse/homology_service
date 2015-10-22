93f5c3e3fe8259e6cc37678d414869bd666247e2
brettin <brettin@anl.gov>
2015-10-22 14:33:25 -0500

    removing temporary script

--- scripts/megahit_project_leone.sh
+++
@@ -1,8 +0,0 @@
-base=b5b8803602f6f05e66d4e737881ae177
-
-/home/brettin/local/megahit-1.0.2/megahit -m 120000000000 --cpu-only -l 300 -o megahit-$base \
-   --input-cmd "zcat *.gz" \
-   --k-min 21 --k-max 99 --k-step 10 \
-   --min-count 2 \
-   --num-cpu-threads 24 \
-   | tee Log
